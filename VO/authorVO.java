fb0fdb95cc9527046cc86d5e402f96882364a9d5
LunardH <76969063+LunardH@users.noreply.github.com>
2021-08-19 20:17:34 -0300

    Create authorVO.java

---
+++ VO/authorVO.java
@@ -0,0 +1,56 @@
+package VO;
+
+public class authorVO {
+
+    private int iDUsuario;
+    private String nome;
+    private String endereco;
+    private String cPF;
+    private String email;
+    private String senha; 
+    private int iDAUTHOR;
+
+    public int getIDusuario(){
+        return this.iDUsuario;
+    }   
+    
+    public void setNome(String nomeUser){
+        this.nome = nomeUser;
+    }
+
+    public String getNome(){
+        return this.nome;
+    } 
+
+    public void setEndereco(String enderecoUser){
+        this.endereco = enderecoUser;
+    }
+
+    public String getEndereco(){
+        return this.endereco;
+    } 
+
+    public String getCPF(){
+        return this.cPF;
+    } 
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getEmail(){
+        return this.email;
+    } 
+
+    public void setSenha(String senhaUser){
+        this.senha = senhaUser;
+    }
+
+    public String getSenha(){
+        return this.senha;
+    } 
+
+    public int getIDAUTHOR(){
+        return this.iDAUTHOR;
+    } 
+}
