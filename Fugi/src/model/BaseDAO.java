e6104e5d4fb16e829b8d76ea900a9765c7892d59
DanielVII <62727519+DanielVII@users.noreply.github.com>
2021-09-16 17:58:13 -0300

    DAO

---
+++ Fugi/src/model/BaseDAO.java
@@ -0,0 +1,25 @@
+package src.model.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseDAO {
+    Connection conectar = null;
+    
+    String url = "jdbc:mariadb://localhost:3306";
+    String user = "daniel_2357";
+    String password = "123";
+
+    public Connection getConnection(){
+        if(conectar == null) {
+            try{
+                conectar = DriverManager.getConnection(url, user, password);
+            }catch(SQLException erro) {
+                erro.printStackTrace();
+            }
+            return conectar;
+
+        } else return conectar;
+    }
+}
