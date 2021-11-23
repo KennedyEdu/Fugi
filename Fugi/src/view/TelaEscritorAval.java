8f690a9c6d69547c4a491465b89d0279c247df2f
KennedyEdu <89218572+KennedyEdu@users.noreply.github.com>
2021-11-23 14:07:41 -0300

    Add files via upload

---
+++ Fugi/src/view/TelaEscritorAval.java
@@ -0,0 +1,22 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TelaEscritorAval extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("VE/TelaEscritorAvaliação.fxml"));
+
+        Scene cena = new Scene(root);
+
+        primaryStage.setTitle("Projeto FUGI");
+        primaryStage.setScene(cena);
+        primaryStage.show();
+
+    }
+}
